9ba0049c417e704a7ea338725af24afa8ce3dd5a
Wolf <wolf@Wolfs-MacBook-Pro.local>
2025-08-17 11:50:53 -0400

    CI: deploy to gh-pages on prod | add lint/type-check gates

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
+const basePath = isGithubActions ? `/${repoName}` : "";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath,
+  assetPrefix: basePath,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
